b9bb2a4ddad6c787771313757741909eaff35d68
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-08-28 18:41:54 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+    
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    
+    
+    formatted_sequence = '[' + ', '.join(map(str, fib_sequence)) + ']'
+    print(formatted_sequence + '')
